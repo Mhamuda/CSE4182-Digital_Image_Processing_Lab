08e2ebc051cfa421ade99446ab459672bab3b410
Mhamuda <53711012+Mhamuda@users.noreply.github.com>
2023-10-31 03:21:04 +0600

    Updated 2(b)

--- 2(b)Differentiate_between_PowerLow&InverseLogarithmicTransform.py
+++ 2(b)Differentiate_between_PowerLow&InverseLogarithmicTransform.py
@@ -1,43 +1,42 @@
-import cv2  # necessary libraries for cv2 for OpenCV functions &
-import numpy as np  #for numerical operations
+import cv2
 import matplotlib.pyplot as plt
+import numpy as np
 
-image_path = './pikachu.png'
+image_path = './Aerial Image.tif'
 gray_image = cv2.imread(image_path,0)
 image = cv2.resize(gray_image,(512,512))
 
-gamma = 2 #gamma value for the powe law transformation.The gamma value controls the amount of gamma correction applied to the image. Adjust this value to achieve different results in terms of brightness and contrast.
+power_law = image.copy()
+inverse_log = image.copy()
+gamma = 0.5     #gamma value for the powe law transformation.The gamma value controls the amount of gamma correction applied to the image. Adjust this value to achieve different results in terms of brightness and contrast.
 
-c = 1   #255/(np.log(1+np.max(image)))   #define parameter c for inverse logarithmic transformation
+power_law = np.power(power_law/255.0,gamma)*255.0       #It raises each pixel value in the image to the power of gamma, scales the result to the range [0, 255]
+power_law = np.uint8(power_law)     #pixel values are always non negative so we use unsigned 8-bit integer(uint8)
 
-power_law_trasnformed = np.power(image/255.0,gamma)*255.0   #It raises each pixel value in the image to the power of gamma, scales the result to the range [0, 255]
-power_law_trasnformed = np.uint8(power_law_trasnformed)
-#pixel values are always non negative so we use unsigned 8-bit integer(uint8)
+c = 255.0/np.log(1+255.0)
+inverse_log = np.exp(inverse_log/c)-1
+inverse_log = np.uint8(inverse_log)
 
-inverse_log_transformed = c*np.log(1+image/255.0)*255.0
-inverse_log_transformed = np.uint8(inverse_log_transformed)
-
-diff_image = cv2.absdiff(power_law_trasnformed,inverse_log_transformed)
-
+diff_image = cv2.absdiff(power_law,inverse_log)
+diff_image = np.uint8(diff_image)
 
 plt.figure(figsize=(7,7))
+plt.subplots_adjust(left=0.1,right=0.9,bottom=0.1,top=0.9,wspace=0.4,hspace=0.4)
+
 plt.subplot(2,2,1)
-plt.title('Original Image')
+plt.title('Original image')
 plt.imshow(image,cmap='gray')
 
 plt.subplot(2,2,2)
-plt.title('Power Law Transform')
-plt.imshow(power_law_trasnformed,cmap='gray')
+plt.title('Powe law image')
+plt.imshow(power_law,cmap='gray')
 
 plt.subplot(2,2,3)
-plt.title('Inverse Logarithmic Transform')
-plt.imshow(inverse_log_transformed,cmap='gray')
+plt.title('Inverse logarithmic image')
+plt.imshow(inverse_log,cmap='gray')
 
 plt.subplot(2,2,4)
-plt.title('Difference Image')
+plt.title('Difference image')
 plt.imshow(diff_image,cmap='gray')
 
 plt.show()
-
-cv2.waitKey(1000)
-cv2.destroyAllWindows()
